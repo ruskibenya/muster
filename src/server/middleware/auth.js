3135ab8f24e1ff2c68563540a69baf28ee5c2408
deanshmuel <dshmuel110@gmail.com>
2019-12-25 11:02:33 +0200

    added beginning of a server with user login,logout and register paths

---
+++ src/server/middleware/auth.js
@@ -0,0 +1,20 @@
+const { User } = require('../../models/user');
+
+//middleware to check if the user is authenticated by querying its token.
+// if authenticated execute the "next" method
+let auth = (req,res,next) => {
+    let token = req.cookies.muster_auth;
+
+    User.findByToken(token,(err,user) => {
+        if(err) throw err;
+        if(!user) return res.json({
+            isAuth: false,
+            error:true
+        });
+        req.token = token;
+        req.user = user;
+        next();
+    })
+}
+
+module.exports = { auth }
---
+++ src/server/server.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.DATABASE);
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+// Models
+const { User } = require('./models/user');
+
+
+//middlwares
+const {auth} = require('./middleware/auth');
+const {admin} = require('./middleware/admin');
+
+//=================================
+//           USERS
+//=================================
+
+app.get('/api/users/auth',auth,(req,res)=>{
+    res.status(200).json({
+        isGuide: req.user.tour_guide,
+        isAuth: true,
+        email: req.user.email,
+        first_name: req.user.first_name,
+        last_name: req.user.last_name,
+    })
+})
+
+app.get('/api/users/logout',auth,(req,res)=>{
+    User.findOneAndUpdate(
+        {_id:req.user._id},
+        {token:''},
+        (err,doc)=>{
+            if(err) return res.json({success:false,err});
+            return res.status(200).send({
+                success:true,
+            })
+        }
+    )
+})
+
+app.post('/api/users/register',(req,res)=>{
+    const user = new User(req.body);
+
+    user.save((err,doc)=>{
+        if(err) return res.json({success:false,err});
+        res.status(200).json({
+            success:true,
+            //userdata:doc,
+        })
+    });
+})
+
+app.post('/api/users/login',(req,res)=>{
+    //find email
+    User.findOne({'email':req.body.email},(err,user)=>{
+        if(!user) return res.json({loginSuccess:false,mesage:'email not found'});
+        //check password
+        user.comparePassword(req.body.password,(err,isMatch)=>{
+            if(!isMatch) return res.json({loginSuccess:false,message:'email or password were incorrect'});
+            // generate token
+            user.generateToken((err,user)=>{
+                if(err) return res.status(400).send(err);
+                res.cookie('muster_auth',user.token).status(200).json({
+                    loginSuccess:true
+                })
+            })
+
+        })
+    })
+    
+
+})
+
+const port = process.env.PORT || 3302;
+
+app.listen(port,()=>{console.log(`server running on ${port}`)});
+
+
